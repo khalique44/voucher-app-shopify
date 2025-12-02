c99ca5e12f6e17869fc314eaca78879b052be4d1
Khalique Ahmed Khan <khalique.ahmed3@gmail.com>
2025-12-03 02:26:28 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const PDFDocument = require('pdfkit');
 const fs = require('fs');
 
 const app = express();
-const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('Voucher PDF App Running');
